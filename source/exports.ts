afca776d03ebfe351f9ea83a3dd96000c75301f7
Upsided <matthewlloydw@gmail.com>
2022-10-02 21:07:33 +0000

    feat: add default export

---
+++ source/exports.ts
@@ -0,0 +1,18 @@
+// Classes
+export * from './classes/stopwatch.js';
+
+// Math/Number
+export * from './functions/round.js';
+export * from './functions/randomInt.js';
+
+// String
+export * from './functions/titleCase.js';
+
+// Array
+export * from './functions/noDuplicates.js';
+export * from './functions/secureShuffle.js';
+export * from './functions/shuffle.js';
+export * from './functions/choice.js';
+
+// Files
+export * from './functions/ls.js';
